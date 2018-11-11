18835621d9ba13db6bb64a5a7cf85d14b005f1a7
Tim Elgersma <timothy.elgersma@gmail.com>
2018-11-11 15:16:02 -0500

    changed Base to db.Model

--- app/models/plays.py
+++ app/models/plays.py
@@ -1,11 +1,8 @@
 from sqlalchemy import Column, ForeignKey, Integer
 
-from . import Base
+from .. import db
 
-
-class Play(Base):
-    __tablename__ = 'plays'
-
+class Play(db.Model):
     id = Column(Integer, nullable=False, primary_key=True)
 
     round_id = Column(Integer, ForeignKey('tricks.id', ondelete='CASCADE'), nullable=False)
--- app/models/tricks.py
+++ app/models/tricks.py
@@ -1,12 +1,10 @@
 from sqlalchemy import Column, ForeignKey, Integer
 
-from . import Base
+from .. import db
 from .columns.suit import Suit
 
 
-class Trick(Base):
-    __tablename__ = 'tricks'
-
+class Trick(db.Model):
     id = Column(Integer, nullable=False, primary_key=True)
 
     dealer_id = Column(Integer, ForeignKey('users.id', ondelete='CASCADE'), nullable=False)
