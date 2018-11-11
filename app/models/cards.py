d4d5e058cf0f16129842938eb37f50dd9c4dc940
Tim Elgersma <timothy.elgersma@gmail.com>
2018-11-11 11:30:42 -0500

    rounds, hands

--- app/models/cards.py
+++ app/models/cards.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, String, Integer 
+from sqlalchemy import Column, ForeignKey, String, Integer
+from sqlalchemy.orm import relationship
 
 from euchre-game.app.models.columns import Suit
 
@@ -8,3 +9,6 @@ class Card(Base):
     id = Column(Integer, nullable=False)
     name = Column(String, nullable=False)
     suit = Column(Suit, nullable=False)
+
+    hand_id = Column(Integer, ForeignKey('hands.id', ondelete='CASCADE'), nullable=True)
+    hand = relationship('hands', back_populates='cards')
