0f07a5265a0acc57eccb302c06693117da3824f6
Tim Elgersma <timothy.elgersma@gmail.com>
2018-11-11 14:46:49 -0500

    newlines

--- app/models/base.py
+++ app/models/base.py
@@ -14,7 +14,6 @@ def init_db():
     # import all modules here that might define models so that
     # they will be registered properly on the metadata.  Otherwise
     # you will have to import them first before calling init_db()
-    
+
     from . import *
     Base.metadata.create_all(bind=engine)
-    
